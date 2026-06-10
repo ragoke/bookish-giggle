0aa28c63a3d82eba2d737b58c6ccac6fe04ea933
Ragoke <67712287+ragoke@users.noreply.github.com>
2026-06-10 19:29:38 +0300

    Hide backend and setup Nginx reverse proxy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // Всі запити будуть починатися з /api
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  // Слухаємо на 0.0.0.0, щоб внутрішня Docker мережа могла достукатись
+  await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
 }
 bootstrap();
